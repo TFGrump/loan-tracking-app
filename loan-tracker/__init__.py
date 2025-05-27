d7ce30e5f9804ffabe84ad0680496edeeba042be
tj <thebuttercat@gmail.com>
2025-05-27 12:36:24 -0400

    Started working on the Flask service.
    Cleaned up the file structure and created the login page.

---
+++ loan-tracker/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask, render_template, send_from_directory, request
+
+from .auth import login
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, 'loan_tracker.sqlite')
+    )
+
+    login_handler = login.Login()
+
+    @app.get('/login')
+    def login_get():
+        return render_template('auth/login.html')
+    
+    @app.post('/login')
+    def login_post():
+        username = request.form['username']
+        password = request.form['password']
+        login_handler.valid_login(username, password)
+        return render_template('auth/login.html', error="Wrong Username and/or Password.")
+    
+    @app.get('/favicon.ico')
+    def get_favicaon():
+        return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+    return app
---
+++ loan-tracker/auth/login.py
@@ -0,0 +1,7 @@
+class Login:
+    def __init__(self):
+        pass
+
+    def valid_login(self, username, password):
+        print(username, password)
+        return False
